e77086b89c0d6132bcfa9680a74ac3f188576524
田中 健太 <kenta@kenta.lan>
2013-08-25 22:03:48 +0900

    initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FirstApp::Application.config.secret_key_base = 'a06f8ef043c3716a228856fdd053e17e48350744e3074ac86bf2349c9e20302ed7d76ead89335eff84c52264924fb43b8935fd7fffb369d788c0e341bcb4856e'
